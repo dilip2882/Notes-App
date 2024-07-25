f708c60bcada4c7c2be071883a175efbe053ac38
Dilip Patel <pateldilip2902@gmail.com>
2024-07-25 15:50:43 +0530

    models and mongoose

--- NotesAPI/src/index.js
+++ NotesAPI/src/index.js
@@ -3,6 +3,8 @@ const userRouter = require("./routes/userRoutes");
 const noteRouter = require("./routes/noteRoutes");
 const app = express();
 
+const mongoose = require("mongoose");
+
 app.use("/users", userRouter);
 app.use("/note", noteRouter);
 
@@ -10,7 +12,14 @@ app.get("/", (req, res) => {
     res.send("hello")
 })
 
-app.listen(5000, ()=>{
-    console.log("Server started on port no. 5000");
+mongoose.connect("mongodb+srv://dspkids2092:<Y5hRiFKDwKimxCUR>@cluster0.55ozhm0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=>{
+    app.listen(5000, ()=>{
+        console.log("Server started on port no. 5000");
+    })
 })
+.catch((errror)=>{
+    console.log(errror);
+});
+
 
